2f43e3ae4eb22cb53ce9ab7b84e0a740c29a024a
Andrea Carriero <andrea@cyberianetwork.com>
2018-02-22 19:55:04 +0100

    Fix logger

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import logging
+
 from toolbox.conf_loader import RootConfiguration
-from toolbox.logger import logger
+from toolbox.logger import get_logger
 
 conf = RootConfiguration()
+log = get_logger(__name__)
+
+log.info("Starting root app...")
--- toolbox/logger.py
+++ toolbox/logger.py
@@ -1,6 +1,4 @@
-import queue
 import logging
-import logging.handlers
 
 from toolbox.conf_loader import RootConfiguration
 
@@ -8,20 +6,11 @@ conf = RootConfiguration()
 
 LOG_LEVEL = logging.INFO
 
-log_formatter = conf.get('logFormatter')
-log_queue = queue.Queue(-1)
-queue_handler = logging.handlers.QueueHandler(log_queue)
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(LOG_LEVEL)
-stream_handler.setFormatter(log_formatter)
-file_handler = logging.FileHandler(conf.get('logFilePath'))
-file_handler.setLevel(LOG_LEVEL)
-file_handler.setFormatter(log_formatter)
-stream_queue_listener = logging.handlers.QueueListener(log_queue, stream_handler)
-file_queue_listener = logging.handlers.QueueListener(log_queue, file_handler)
-stream_queue_listener.start()
-file_queue_listener.start()
+logging.basicConfig(
+    format = conf.get('logFormatter'),
+    level = LOG_LEVEL
+)
 
-logger = logging.getLogger(__name__)
-logger.addHandler(queue_handler)
-logger.setLevel(LOG_LEVEL)
+def get_logger(module_name):
+    logger = logging.getLogger(module_name)
+    return logger
