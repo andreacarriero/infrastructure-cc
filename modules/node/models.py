bf30dbe669bf506e38432e08b1076ef78cbe79ad
Andrea Carriero <andrea@cyberianetwork.com>
2018-02-22 20:43:02 +0100

    Add node's ip module

--- modules/node/models.py
+++ modules/node/models.py
@@ -15,3 +15,19 @@ class Node(db.Model):
             'type': self.type,
             'datacenter_id': self.datacenter_id
         }
+
+class NodeIP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    node_id = db.Column(db.ForeignKey(Node.id))
+    ipv = db.Column(db.Integer)
+    ip = db.Column(db.String(46))
+    netmask = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'node_id': self.node_id,
+            'ipv': self.ipv,
+            'ip': self.ip,
+            'netmask': self.netmask
+        }
