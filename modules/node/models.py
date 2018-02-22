e3212d15ef44acddde5ae789401a32d81e89c6c8
Andrea Carriero <andrea@cyberianetwork.com>
2018-02-22 20:59:33 +0100

    Add node status

--- modules/node/models.py
+++ modules/node/models.py
@@ -11,13 +11,15 @@ class Node(db.Model):
     def serialize(self):
         datacenter = Datacenter.query.filter_by(id = self.datacenter_id).first()
         ips = NodeIP.query.filter_by(node_id = self.id).all()
+        status = NodeStatus.query.filter_by(node_id = self.id).order_by('-last_update').first()
 
         return {
             'id': self.id,
             'name': self.name,
             'type': self.type,
-            'datacenter': datacenter.serialize(),
-            'ips': [ip.serialize() for ip in ips]
+            'datacenter': (lambda datacenter: datacenter.serialize() if datacenter else None)(datacenter),
+            'ips': [ip.serialize() for ip in ips],
+            'status': (lambda status: status.serialize() if status else None)(status)
         }
 
 class NodeIP(db.Model):
@@ -34,4 +36,20 @@ class NodeIP(db.Model):
             'ipv': self.ipv,
             'ip': self.ip,
             'netmask': self.netmask
+        }
+
+class NodeStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    node_id = db.Column(db.ForeignKey(Node.id))
+    last_update = db.Column(db.DateTime, server_default=db.func.now())
+    imposed_status = db.Column(db.String(20))
+    current_status = db.Column(db.String(20))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'node_id': self.node_id,
+            'last_update': str(self.last_update),
+            'imposed_status': self.imposed_status,
+            'current_status': self.current_status
         }
