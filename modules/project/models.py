ffe0c3ffb7ba444c17140c69d35f192c34bbd949
Andrea Carriero <andrea@cyberianetwork.com>
2018-02-22 23:00:53 +0100

    Add details to project serialization

--- modules/project/models.py
+++ modules/project/models.py
@@ -1,5 +1,6 @@
 from toolbox.database import db
 from modules.user.models import User
+from modules.node.models import Node
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,8 +8,28 @@ class Project(db.Model):
     name = db.Column(db.String(100))
 
     def serialize(self):
+        user = User.query.filter_by(id=self.user_id).first()
+        nodes_links = ResourceNodeLink.query.filter_by(project_id=self.id).all()
+
         return {
             'id': self.id,
-            'user_id': self.user_id,
-            'name': self.name
+            'user': (lambda user: user.serialize() if user else None)(user),
+            'name': self.name,
+            'nodes': [link.serialize_node() for link in nodes_links]
+        }
+
+class ResourceNodeLink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.ForeignKey(Project.id))
+    node_id = db.Column(db.ForeignKey(Node.id))
+
+    def serialize_node(self):
+        node = Node.query.filter_by(id=self.node_id).first()
+        return node.serialize()
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'project_id': self.project_id,
+            'node_id': self.node_id
         }
