7f75438b72f5589301978cbfe3301ca5731f89e3
Andrea Carriero <andrea@cyberianetwork.com>
2018-02-25 16:42:16 +0100

    View node commands per project job

--- modules/project/admin_api.py
+++ modules/project/admin_api.py
@@ -5,7 +5,7 @@ from flask_restful import Api, Resource, reqparse
 from toolbox.logger import get_logger
 from modules.project.models import Project, ResourceNodeLink, ProjectCommandJob
 from modules.user.models import User
-from modules.node.models import Node
+from modules.node.models import Node, NodeCommand
 
 log = get_logger(__name__)
 
@@ -45,5 +45,25 @@ class ProjectResource(Resource):
             'project_command_jobs': [job.serialize() for job in project_command_jobs]
         }
 
+class ProjectCommandJobResource(Resource):
+    """
+    Manages specific command job
+    """
+
+    def get(self, project_id, job_id):
+        log.info("Getting job ID:%d", job_id)
+
+        job = ProjectCommandJob.query.filter_by(id=job_id).first()
+        if not job:
+            abort(404, "Project Command Job not found")
+
+        node_commands = NodeCommand.query.filter_by(project_command_job_id=job_id).all()
+
+        return {
+            'project_command_job': job.serialize(),
+            'node_commands': [command.serialize() for command in node_commands]
+        }
+
 api.add_resource(RootResource, '/', endpoint='projects')
 api.add_resource(ProjectResource, '/<int:project_id>', endpoint='project')
+api.add_resource(ProjectCommandJobResource, '/<int:project_id>/jobs/<int:job_id>', endpoint='project_command_job')
